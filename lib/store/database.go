19658327fd2f46a8f4f2786bdd7b552202fbe956
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-10-28 19:30:01 +0900

    define store interface

--- lib/store/database.go
+++ lib/store/database.go
@@ -1,3 +1,29 @@
 package store
 
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/kaito2/wiser-go/lib"
+)
 
+// WiserStore interface
+type WiserStore interface {
+	InitDatabase(dbPath string) error
+	FinDatabase() error
+	GetDocumentID(title string) (int, error)
+	GetDocumentTitle(documentID int) (string, error)
+	AddDocument(title, body string) error
+	// TODO: Named return value only here
+	GetTokenID(token string, insert bool) (tokenID, docCount int, err error)
+	GetToken(tokenID int) (string, error)
+	GetPostings(tokenID int) (wiser.PostingsList, error)
+	UpdatePostings(tokenID int, docsCount int, postingsList wiser.PostingsList)
+	// Get setting info from DB
+	GetSetting(key string) (string, error)
+	ReplaceSettings(key, value string) error
+	GetDocumentCount() (int, error)
+	// TODO: survey how to implement in golang + sqlite
+	// start DB transaction
+	Begin()
+	Commit()
+	RollBack()
+}
